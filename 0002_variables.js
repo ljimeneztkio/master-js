1610024fb6c700ff951807dc5de462dc9f77e99d
Luis Enrique Jimenez Trejo <ljimenezt@kio.tech>
2025-08-21 20:23:50 -0600

    Primera leccion de variables, declaramos e imprimimos

---
+++ 0002_variables.js
@@ -0,0 +1,11 @@
+let nombre = "Juan";
+let edad = 30;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+
+let miNumero1 = 15;
+console.log(miNumero1);
+
+let miNumero2 = 20;
+console.log(miNumero2);
