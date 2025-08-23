443cbe73e46a49f77dbff16378271630f24524dc
Luis Enrique Jimenez Trejo <ljimenezt@kio.tech>
2025-08-22 23:15:04 -0600

    Reglas para nombrar variables

---
+++ 0003_reglas_de_nombres.js
@@ -0,0 +1,18 @@
+//Las variables pueden iniciar con $, _ o una letra pero no con un número
+let $miNumero1 = 21;
+console.log($miNumero1);
+
+let _miNumero2 = 22;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
+
+//JavaScript es case sensitive
+/**
+ * Comentario multilínea
+ */
+
+/*
+Todo lo que está aquí dentro es un comentario
+*/
