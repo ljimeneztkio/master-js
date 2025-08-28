e92ef3992b5b0dee78afb72ed894d89f11ce0c4a
Luis Enrique Jimenez Trejo <ljimenezt@kio.tech>
2025-08-27 21:11:05 -0600

    Operador Typeof

---
+++ 0005_operador_typeof.js
@@ -0,0 +1,23 @@
+let miNumero = 10;
+console.log(miNumero);
+console.log(typeof miNumero);
+
+let miFlotante = 100.009;
+console.log(miFlotante);
+console.log(typeof miFlotante);
+
+let miCadena = "Hola, soy una cadena de texto";
+console.log(miCadena);
+console.log(typeof miCadena);
+
+let miBooleano = true;
+console.log(miBooleano);
+console.log(typeof miBooleano);
+
+let miNull = null;
+console.log(miNull);
+console.log(typeof miNull);
+
+let miUndefined = undefined;
+console.log(miUndefined);
+console.log(typeof miUndefined);
