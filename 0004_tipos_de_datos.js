a0d5c39c6ea9fbe8470e91870c838748cce3b7d9
Luis Enrique Jimenez Trejo <ljimenezt@kio.tech>
2025-08-27 21:04:36 -0600

    Clase: Tipos de datos en JS

---
+++ 0004_tipos_de_datos.js
@@ -0,0 +1,17 @@
+let miNumero = 10;
+console.log(miNumero);
+
+let miFlotante = 100.009;
+console.log(miFlotante);
+
+let miCadena = "Hola, soy una cadena de texto";
+console.log(miCadena);
+
+let miBooleano = true;
+console.log(miBooleano);
+
+let miNull = null;
+console.log(miNull);
+
+let miUndefined = undefined;
+console.log(miUndefined);
