ad480c29b008baf2e6e36eef8813be2c08248cdc
ljimenezt <ljimenezt@kio.tech>
2025-10-13 13:29:05 -0600

    Se agrega practica 0008 sobre operadores aritmeticos y se añade index.html para debuggear tras la deshabilitación de Quokka

---
+++ 0008-operadores_aritmeticos.js
@@ -0,0 +1,41 @@
+/**
+ * Los operadores aritmeticos me permiten realizar operaciones matematicas basicas
+ * como suma, resta, multiplicación, división, exponente, incremento, decremento
+ * (+, -, *, /, **, %, ++, --)
+ */
+
+
+let a, b, c, d, e;
+
+a = 10;
+b = 20;
+c = 30;
+d = 40;
+e = 50;
+
+// Suma
+console.log(a + b); // 10  + 20 = 30
+
+// Reste
+console.log(b - c); // 20 - 30 = -10
+
+// Multiplicacion
+console.log(c * d); // 30 * 40 = 1200
+
+// Division
+console.log(d / e); // 40 / 50 = 0.8
+
+// Potencia
+console.log(a ** 2); // 10 ** 2 = 100
+
+// Modulo
+console.log(b % c); // 20 % 30 = 20
+console.log(c % b); // 20 % 30 = 10 !El orden importa
+
+// Incremento
+console.log(d++); // imprime el valor 40 y suma 1 despues de imprimir
+console.log(d); // Valor actualizado: 41
+
+// Decremento
+console.log(e--); //imprime el valor 50 y resta 1 despues de imprimir
+console.log(e); // Valor actualizado: 49
