05c948551b4b3872b36434d2db1ab427541ffeac
ljimenezt <ljimenezt@kio.tech>
2025-10-13 14:04:20 -0600

    Se agrega la practica de operadores de asignacion

---
+++ 0010-operadores_asignacion.js
@@ -0,0 +1,22 @@
+let miNumero = 20;
+
+miNumero += 5;
+console.log(miNumero);
+
+miNumero -= 3;
+console.log(miNumero);
+
+miNumero *= 2;
+console.log(miNumero);
+
+miNumero /= 4;
+console.log(miNumero);
+
+miNumero %= 2;
+console.log(miNumero);
+
+miNumero = 2;
+
+miNumero **= 10;
+console.log(miNumero);
+
