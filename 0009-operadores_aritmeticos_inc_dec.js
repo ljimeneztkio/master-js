97f23cf9716d3bcef346689806bfc264483aa51c
ljimenezt <ljimenezt@kio.tech>
2025-10-13 13:51:12 -0600

    Se agrega archivo de incrementos y decrementos, pre y post

---
+++ 0009-operadores_aritmeticos_inc_dec.js
@@ -0,0 +1,17 @@
+let d = 0;
+let e = 0;
+
+// pre - incremento;
+++d;
+console.log(d);
+
+// post - incremento
+d++; // imprime el valor 40 y suma 1 despues de imprimir
+console.log(d); // Valor actualizado: 41
+
+// pre - decremento
+--d;
+console.log(d);
+
+d--;
+console.log(d);
