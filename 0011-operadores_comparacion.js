ec19b9b0c26616df40f20210b0fc64d4c8a31ff7
ljimenezt <ljimenezt@kio.tech>
2025-10-13 16:06:57 -0600

    Se agrega archivo de practicas de operadores de comparacion

---
+++ 0011-operadores_comparacion.js
@@ -0,0 +1,24 @@
+/**
+ * Sirven para evaluar si dos valores cumplen con alguna condicion especificas indicadas
+ * Igual a == 
+ * Igual a === (Evalua tipo de dato y valor)
+ * Distinto a != 
+ * Distinto a !== (Evalua el tipo de dato y valor)
+ * Mayor que >
+ * Mayor o igual que >=
+ * Menor que <
+ * Menor o igual que <=
+ */
+
+let a = 10;
+let b = '10';
+
+
+console.log(a == b);
+console.log(a === b);
+console.log(a > b);
+console.log(a >= b);
+console.log(a < b);
+console.log(a <= b);
+
+console.log(`${a} == ${b} -->`, a == b);
